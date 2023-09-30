8a51506b1b1355dbbd484a5f44fc742dc20b3a81
Altheo Saquilayan <altheo.saquilayan@exist.com>
2023-09-30 13:27:09 +0800

    Added cors

--- part03/backend/index.js
+++ part03/backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
@@ -87,4 +88,5 @@ const generateId = () => {
     console.log(`Server running on port ${PORT}`)
   })
 
+  app.use(cors())
   app.use(express.static('dist'))
